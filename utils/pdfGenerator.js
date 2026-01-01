a8eeb55bc5149f932acea6d07184b63374f3e532
umairdev58 <umair.dev58@gmail.com>
2026-01-01 11:39:28 +0500

    Update pdfGenerator.js

--- utils/pdfGenerator.js
+++ utils/pdfGenerator.js
@@ -68,7 +68,7 @@ class PDFGenerator {
     // Sale marka (left/top area) positioned above customer name
     if (sale.marka) {
       this.doc.fontSize(7).font('Helvetica-Bold');
-      this.doc.text(sale.marka, 10, 40, { width: 260 });
+      this.doc.text(sale.marka, 0, 60, { width: 260 });
     }
 
     // Customer name (left area)
